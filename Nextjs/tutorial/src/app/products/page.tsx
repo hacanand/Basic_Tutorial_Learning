b751e0b8da44f975e3a3f2e14a58ba0badf1e01b
anand <anandpromax@gmail.com>
2024-03-25 19:59:14 +0530

    Remove unused CSS and update metadata

---
+++ Nextjs/tutorial/src/app/products/page.tsx
@@ -0,0 +1,11 @@
+export default function ProductList() {
+    return (
+        <>
+            <h1>Product List</h1>
+            <h1>product 1</h1>
+            <h1>product 2</h1>
+            <h1>product 3</h1>
+
+        </>
+    )
+}
